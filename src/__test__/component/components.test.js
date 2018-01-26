898b8f64dfe1fff8c22072d55eede0589689cdb9
dalhun namgung <bnamgung@gmail.com>
2018-01-25 18:33:38 -0800

    working on

--- src/__test__/component/components.test.js
+++ src/__test__/component/components.test.js
@@ -8,7 +8,7 @@ import ExpenseForm from '../../component/expense-form';
 
 describe('<CategoryForm/>', () => {
   test('add a new category', () => {
-    const wrapper = (<CategoryForm />);
+    const wrapper = shallow(<CategoryForm />);
 
     let name = "groceries";
     let budget = "100";
